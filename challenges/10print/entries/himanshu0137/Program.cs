9b6301c8c173ff62d7fe8ac35b8535f6dda42566
Himanshu Bansal <himanshu.bansal@softwaysolutions.com>
2019-06-24 11:16:54 +0530

    C# entry for 10 print

---
+++ challenges/10print/entries/himanshu0137/Program.cs
@@ -0,0 +1,2 @@
+﻿using System;
+namespace _10print { class Program { static void Main(string[] args) { Console.OutputEncoding = System.Text.Encoding.UTF8; for (; ; ) Console.Write("╲╱"[new Random().Next() & 1]); } } }
