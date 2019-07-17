ade2ad4586de0e4b8b640042ddf38bb35542c25d
Jack Pickus <jpickus@wisc.edu>
2019-07-17 11:24:35 -0500

    Create TenPrint.java

---
+++ challenges/10print/entries/jackpickus/TenPrint.java
@@ -0,0 +1,2 @@
+import java.util.*;
+public class TenPrint {public static void main(String[] args) {String[] a = new String[]{"/", "\\"};while(true){int r = new Random().nextInt(a.length);System.out.print(a[r]);}}}
